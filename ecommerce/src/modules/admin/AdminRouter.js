0cce8dea9c41b78dd1b4a217df0aec1562b1b557
Denisse Montalvo <adc.logix@gmail.com>
2020-11-11 23:19:08 -0500

    Maquetado admin

--- ecommerce/src/modules/admin/AdminRouter.js
+++ ecommerce/src/modules/admin/AdminRouter.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import AdminDashboard from './screens/dashboard/AdminDashboard'
+import AdminProductos from './screens/productos/AdminProductos'
+
+import './../../css/index.css'
+
 
 const AdminRouter = () => {
     return (
         <Switch >
+            <Route path={"/admin/productos"} component={AdminProductos}/> 
             <Route path={"/admin"} component={AdminDashboard}/> 
+            
         </Switch>
     )
 }
